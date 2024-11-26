341bc44df79dc95020f60ce8598cfa769e45694c
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-11-26 20:07:53 +0700

    query builder select

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class QueryBuilderTest extends TestCase
@@ -28,5 +29,17 @@ class QueryBuilderTest extends TestCase
 
         $result = DB::select('select count(id) as total from categories');
         $this->assertEquals(2, $result[0]->total);
+    }
+
+    public function testSelect()
+    {
+        $this->testInsert();
+
+        $collection = DB::table('categories')->select(['id', 'name'])->get();
+        $this->assertNotNull($collection);
+
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
     }
 }
